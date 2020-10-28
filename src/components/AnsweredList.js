3f2679b8244af1fdd34b8a68e987b09561b70b6e
Ryan Deyo <ryan@f1v.co>
2020-10-28 10:23:31 -0400

    add redux and router packages

---
+++ src/components/AnsweredList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AnsweredList extends Component {
+  render () {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default AnsweredList;
---
+++ src/components/CreateQuestion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class CreateQuestion extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default CreateQuestion;
